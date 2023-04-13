02ae635dad0cb6d2a83e8ad1f0f093c211a4848e
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-13 12:39:46 +0300

    Remove contract implementations in another contracts solidity files

--- Practice#3/Task3.sol
+++ Practice#3/Task3.sol
@@ -15,32 +15,3 @@ interface IAttacker {
     function attack(address vault) external;
 }
 
-contract Vault is IVault {
-    mapping(address => uint256) public balance;
-
-    function deposit() external payable {
-        balance[msg.sender] += msg.value;
-    }
-
-    function withdrawSafe(address payable holder) external {
-        // TODO
-    }
-
-    function withdrawUnsafe(address payable holder) external {
-        // TODO
-    }
-}
-
-contract Attacker is IAttacker {
-    function depositToVault(address vault) external payable {
-        IVault(vault).deposit{value: msg.value}();
-    }
-
-    fallback() external payable {
-        // TODO
-    }
-
-    function attack(address vault) external {
-        // TODO
-    }
-}
