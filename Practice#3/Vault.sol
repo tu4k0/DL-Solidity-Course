9cb951e011529ccc11d1ebfba18b02ba8fce4b94
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-17 12:51:52 +0300

    Update vault contract (add interface, internal variable amount to store holder balance)

--- Practice#3/Vault.sol
+++ Practice#3/Vault.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import "Task3.sol";
+interface IVault {
+    function deposit() external payable;
+
+    function withdrawSafe(address payable holder) external;
+
+    function withdrawUnsafe(address payable holder) external;
+}
 
 contract Vault is IVault {
     mapping(address => uint256) public balance;
@@ -11,15 +17,18 @@ contract Vault is IVault {
     }
 
     function withdrawSafe(address payable holder) external {
-        require(balance[holder] > 0, "Holder have no funds");
+        uint256 amount = balance[holder];
+        require(amount > 0, "Holder have no funds");
         balance[holder] = 0;
-        (bool success, ) = holder.call{value: balance[holder]}("");
+        (bool success, ) = holder.call{value: amount}("");
         require(success, "Safe transfer fail");
     }
 
     function withdrawUnsafe(address payable holder) external {
-        (bool success, ) = holder.call{value: balance[holder]}("");
-        require(success, "Unsafe transfer fail");
+        uint256 amount = balance[holder];
+        require(amount > 0, "Holder have no funds");
+        (bool success, ) = holder.call{value: amount}("");
         balance[holder] = 0;
+        require(success, "Unsafe transfer fail");
     }
 }
