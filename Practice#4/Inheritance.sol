dc11d03c53e7eef4ae03c73cdaeee3063f9cdb2b
tu4k0 <kirya.mlievskij.00@gmail.com>
2023-04-23 23:53:27 +0300

    Add Task4 solution (Contract D realization)

--- Practice#4/Inheritance.sol
+++ Practice#4/Inheritance.sol
@@ -1,8 +1,56 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.19;
 
-contract Inheritance {
-    constructor(){
+contract A {
+    uint256 public a = 1;
+    uint256 private deposited;
 
+    constructor() {
+        possibleRevert();
+    }
+
+    function deposit(uint256 amount) public virtual {
+        require(amount > 0, "Amount is zero");
+
+        deposited = amount;
+    }
+
+    function getDeposited() external view returns (uint256) {
+        return deposited;
+    }
+
+    function possibleRevert() public pure virtual {
+        revert("Oops");
+    }
+}
+
+contract B is A {
+    uint256 private b = a += 10;
+
+    function deposit(uint256) public virtual override {
+        revert("Deposit is blocked");
+    }
+}
+
+contract C is A {
+    uint256 private c = a *= 2;
+
+    function deposit(uint256 amount) public virtual override {
+        super.deposit(amount);
+    }
+
+    function getC() external view returns (uint256) {
+        return c;
+    }
+}
+
+contract D is C, B {
+
+    constructor() A() {}
+
+    function possibleRevert() public pure override {}
+
+    function deposit(uint256 amount) public virtual override(C, B) {
+        A.deposit(amount);
     }
 }
